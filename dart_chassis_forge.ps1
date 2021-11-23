25c4ba867ea07d4de66fffae35bb615d79544546
Alexander Scott <xander@axrs.io>
2021-11-23 19:38:25 +1000

    Chore: Powershell Shebang added to Helper Script

--- dart_chassis_forge.ps1
+++ dart_chassis_forge.ps1
@@ -1,5 +1,5 @@
-If(!(test-path '.dart_tool') -Or -not(Test-Path -Path 'pubspec.lock' -PathType Leaf))
-{
+#!/usr/bin/env pwsh
+If (!(test-path '.dart_tool') -Or -not(Test-Path -Path 'pubspec.lock' -PathType Leaf)) {
     & dart pub get | Out-Null
 }
 & dart run chassis_forge:build --directory example --main example/main.dart --executable-target kernel --verbose | Out-Null
